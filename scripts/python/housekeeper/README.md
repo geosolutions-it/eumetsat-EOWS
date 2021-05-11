e99e8466c73f3d7ad1aa1c1f74d40a6ba49995b7
Asdrubal Gonzalez <agpenton@gmail.com>
2021-05-11 17:34:17 +0400

    Changes on the environment: - change the logic of the script to get the data, and process it. - adding more documentation to the README.md.

--- scripts/python/housekeeper/README.md
+++ scripts/python/housekeeper/README.md
@@ -20,7 +20,6 @@
    - `RECEIVER`  Email address to where you will send the notifications.  
    - `SENDER`    Email account used to send the notifications.
    - `FIELDS`    Files used for the query.
-   - `T_LIMIT`   Number of days till when you want to keep the data.
    - `TRIES`     The maximum number of attempts.
    - `DELAY`     Initial delay between attempts.
    - `BACKOFF`   Multiplier applied to delay between attempts.
@@ -29,7 +28,7 @@
    - `BATCH`     Amount of data take it in one time.
    - `GLOBAL_RETENTION` Global Retention time for all the layers.
    - `LAYERS`    List of layers to use.
-   - `COLUMN_TIME_NAME` Name of the column where the time is.
+   - `COLUMN_TIME` Name of the column where the time is.
 
 1. to run the script:
     - ```shell
@@ -46,15 +45,30 @@
 
 ### Deployment of the script to kubernetes.
 
-1. Go to the directory where the files for deploy to kubernetes are:
+1. Go to the directory where the files for deploy to Kubernetes are:
     ```shell
     cd k8s/
     ```
-2. Create the configmap for the Script.
+2. The content of the directory should look like follow:
+```shell
+.
+├── .env.example
+├── cleaner-cronjob.yaml
+└── kustomization.yaml
+```
+  - `.env.example` where the environment variables are. 
+  *NOTE* this file need to be modified with the values that you need for your cluster.
+  - `cleaner-cronjob.yaml` the deployment of the cronjob.
+  - `kustomization.yaml` the kustomize file where the whole deployment occurred.
+
+2. Create the full deployment for the Script with the kustomization.yaml file.
   ```shell
-  kubectl create configmap cleaner-config --from-env-file=.env -n chosen-namespace
+  kubectl create -k .
   ```
-3. Create the CronJob to run the Script.
-  ```shell
-  kubectl create -f cleaner-cronJob.yaml
-  ```
+  - the output of that command:
+    ```shell
+    configmap/cleaner-config-855569dg4b created
+    secret/cleaner-db-pass-9dg7b6859m created
+    secret/cleaner-email-pass-bf4gmmd755 created
+    cronjob.batch/imagemosaiccleaner created
+    ```
--- scripts/python/housekeeper/housekeeper.py
+++ scripts/python/housekeeper/housekeeper.py
@@ -12,15 +12,52 @@ import pytz
 from environs import Env
 import logging
 
+# Initializing the environment variables from dotenv file.
 env = Env()
 env.read_env()
-print(env)
 tries = env.int("TRIES")
 delay = env.int("DELAY")
 backoff = env.int("BACKOFF")
 max_delay = env.int("MAX_DELAY")
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
-logger = logging.getLogger(__name__)
+global_retention = env.list("GLOBAL_RETENTION")
+layers_list = env.list("LAYERS")
+time_limit = ""
+fields = env("FIELDS")
+fields_list = env("FIELDS").split(",")
+tz = pytz.timezone(env("TZ"))  # set timezone
+t_now = datetime.now(tz=tz)
+batch = env.int("BATCH")
+
+# configuring the logs format.
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(app_name)s %(levelname)s:%(message)s')
+layer_name = ""
+
+# Creating classes to manipulate the logs.
+class AppFilter(logging.Filter):
+    filter_name = ""
+    def filter(self, record):
+        record.app_name = self.filter_name
+        return True
+
+class OneLineExceptionFormatter(logging.Formatter):
+    def formatException(self, exc_info):
+        result = super().formatException(exc_info)
+        return repr(result)
+
+    def format(self, record):
+        result = super().format(record)
+        if record.exc_text:
+            result = result.replace("\n", "")
+        return result
+# Setting the logger to the systemd.
+handler = logging.StreamHandler()
+formatter = OneLineExceptionFormatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+cleaner = logging.getLogger(layer_name)
+cleaner.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+filter = AppFilter()
+cleaner.addFilter(filter)
+cleaner.addHandler(handler)
 
 
 def connect():
@@ -41,74 +78,87 @@ def connect():
                                       database=env("DB_NAME")
                                       )
 
-        logging.info("connected to database")
+        cleaner.info("connected to database")
         return connection
 
     except (Exception, Error) as error:
-        logging.critical("Error while connecting to PostgreSQL", error)
+        cleaner.exception("Error while connecting to PostgreSQL", error)
         raise
 
 
-# use retry decorator to retry the make many attempts as need it if the delete failed.
 @retry((ValueError, TypeError), tries=tries, delay=delay, backoff=backoff, max_delay=max_delay)
-def getdata(conn):
+def getdata(conn, time_limit, schema, table, col_time_name):
     """
     :param conn: connection parameter returned from connect function.
+    :param time_limit: retention time of the records.
+    :param schema: Name of schema of the database where the tables reside.
+    :param table: Name of table where the records reside.
+    :param col_time_name: Name of column where the time records reside.
     :return:
     """
-    # table = env(o_table)
-    table = env.str("TABLE")
-    time_limit = env.int("T_LIMIT")
-    schema = env.str("SCHEMA")
-    fields = env("FIELDS")
-    tz = pytz.timezone(env("TZ"))  # set timezone
-    t_now = datetime.now(tz=tz)
-    date_t = (t_now - timedelta(days=time_limit)).strftime('%Y-%m-%d %H:%M:%S.%f')
-    sql_query = f'SELECT {fields} FROM {schema}."{table}" WHERE \'{date_t}\' > "{table}".ingestion;'
+    date_t = (t_now - timedelta(hours=time_limit)).strftime('%Y-%m-%d %H:%M:%S.%f')
+    sql_query = f'SELECT {fields} FROM {schema}."{table}" WHERE \'{date_t}\' > "{table}".{col_time_name};'
+    conn.autocommit = True
 
     try:
+        # Opening a cursor to query the database.
         with conn.cursor() as curs:
-            logging.info("Quering the database")
+            cleaner.info("Quering the database")
             curs.execute(sql_query)
-            # Looping the data to get the time and the path of the files.
-            batch = env.int("BATCH")
-            for x in curs.fetchall():
-                curs.itersize = batch
-                filename = str(x[0])
-                mod_date = str(x[1])
-                sql_drop = f'DELETE FROM {schema}."{table}" WHERE \'{date_t}\' < "{table}".ingestion;'
-                # sql_show = f'SELECT * FROM {schema}."{table}" WHERE \'{date_t}\' > "{table}".ingestion;'
-
-                with open('files.txt', 'w') as a_writer:
-                    a_writer.write(f'{filename}\n')
-                    a_writer.close()
-                # Comparing the date of the db records to delete the olders than T_LIMIT
-                curs.execute(sql_drop)
-                conn.commit()  # commiting the operation.
-                with open('files.txt', 'r') as a_reader:
-                    for line in a_reader:
-                        os.remove(filename)
-                        logging.info(f'This file have been deleted: {filename}')
-                    a_reader.close()
-                if os.path.exists("files.txt"):
-                    os.remove("files.txt")
+            cleaner.info(curs.statusmessage)
+            statusmessage = curs.statusmessage
+            results = "SELECT 0"
+            if statusmessage != results:
+                batch = env.int("BATCH")
+                # setting the number of rows to be fetched at the time.
+                curs.arraysize = batch
+                for x in curs.fetchmany():
+                    filename = str(x[0])
+                    mod_date = str(x[1])
+                    sql_drop = f'DELETE FROM {schema}."{table}" WHERE \'{date_t}\' > "{table}".{col_time_name};'
+                    # writing the the records in a file to delete after.
+                    with open('files.txt', 'w') as a_writer:
+                        a_writer.write(f'{filename}\n')
+                        cleaner.info(f"writing the {filename} record to delete later")
+                        a_writer.close()
+                    # Comparing the date of the db records to delete the olders than date_t
+                    cleaner.info("deleting the entries in the database")
+                    cleaner.info(curs.execute(sql_drop))
+                    cleaner.info(curs.statusmessage)
+                    with open('files.txt', 'r') as a_reader:
+                        for line in a_reader:
+                            try:
+                                if os.path.exists(filename):
+                                    os.remove(filename)
+                                    cleaner.info(f'This file have been deleted: {filename}')
+                            except (Exception, Error) as er:
+                                cleaner.error(er)
+                        a_reader.close()
+                    if os.path.exists("files.txt"):
+                        cleaner.info("deleting the path container file")
+                        os.remove("files.txt")
+            else:
+                cleaner.info("Nothing to clean. See you on the next Cleaning")
         conn.close()
     except (Exception, Error) as e:
         print(e)
-        logging.error(e)
+        cleaner.error(e)
         with open('errors.txt', 'w') as a_writer:
             a_writer.write(f'Filename Path: \t\t\t timestamp:\n{filename} \t\t\t {mod_date}\n')
             a_writer.close()
         conn.rollback()
         raise e
 
-
 def notification():
+    cleaner.info("creating the email to send")
     subject = "Cleaning Task is finished"
-    body = "This is the file that contains the records that cuold not be deleted"
+    body = "This is the file that contains the records that could not be deleted"
+    body2 = "The Clean process finished without any problems."
+
     sender_email = env("SENDER")
     receiver_email = env("RECEIVER")
     password = env("PASS")
+    filename = "errors.txt"
 
     # Create a multipart message and set headers
     message = MIMEMultipart()
@@ -117,38 +167,66 @@ def notification():
     message["Subject"] = subject
     message["Bcc"] = receiver_email  # Recommended for mass emails
 
-    # Add body to email
-    message.attach(MIMEText(body, "plain"))
+    try:
+        if os.path.exists(filename):
+            cleaner.info("attaching the list of file with errors to send")
+            # Add body to email
+            message.attach(MIMEText(body, "plain"))
 
-    filename = "files.txt"  # In same directory as script
+            with open(filename, 'rb') as attachment:
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
 
-    # Open PDF file in binary mode
-    with open(filename, "rb") as attachment:
-        # Add file as application/octet-stream
-        # Email client can usually download this automatically as attachment
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
+            # Encode file in ASCII characters to send by email
+            encoders.encode_base64(part)
 
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
+            # Add header as key/value pair to attachment part
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {filename}",
+            )
 
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
+            # Add attachment to message and convert message to string
+            message.attach(part)
+        else:
+            message.attach(MIMEText(body2, "plain"))
+    except:
+        cleaner.exception('No file to send')
 
-    # Add attachment to message and convert message to string
-    message.attach(part)
     text = message.as_string()
 
     # Log in to server using secure context and send email
     context = ssl.create_default_context()
+    cleaner.info("Sending the email")
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, text)
 
+def housekeeper():
+    # Setting up the function to start the process.
+    cleaner.info("Starting the Cleaning Process.")
+    column_time = env("COLUMN_TIME")
+    global_retention_time = int(global_retention[1])
+    # getting data from the layer list.
+    for layers in layers_list:
+        layer = layers.split(";")
+        layer_name = layer[0]
+        layer_time = layer[-1]
+        layer_parameters = layer[1].split(".")
+        filter.filter_name = layer_name
+        schema = layer_parameters[0]
+        table = layer_parameters[1]
+        # Implementing the granule housekeeping process configuration.
+        if global_retention_time > 0:
+            cleaner.info(f"Running the Housekeeper script with the Global Retention Time: {global_retention_time}")
+            getdata(conn=connect(), time_limit=global_retention_time, schema=schema, table=table, col_time_name=column_time)
+        else:
+            cleaner.info(f"Running the Housekeeper script with the Layer Retention Time: {layer_time}")
+            getdata(conn=connect(), time_limit=layer_time, schema=schema, table=table, col_time_name=column_time)
+
+    # Sending a email when the job is done.
+    notification()
+    cleaner.info("DONE")
 
 if __name__ == '__main__':
-    getdata(conn=connect())
-    notification()
+    housekeeper()
